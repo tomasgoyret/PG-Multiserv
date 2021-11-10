d5a5a660f21dda36bcc045290530d37f800e8ccc
Ulises Vargas <uli.vargas02@gmail.com>
2021-11-10 09:32:13 -0600

    Gracias Santi

--- Multiserv/api/src/controller/getUsers.js
+++ Multiserv/api/src/controller/getUsers.js
@@ -3,11 +3,15 @@ const db = require("../db.js");
 const getUsers = async(req, res) => {
     try {
         const peticion = await db.collection("usuarios").get();
-        const {docs} = peticion;
-        const usuarios = docs.map(usuario => ({
-            id:usuario.id,
-            datos:usuario.data()
-        }));
+        const { docs } = peticion;
+        
+        const usuarios = docs.map( usuario => {
+            let data = usuario.data()
+            return {
+                id:usuario.id,
+                ...data
+            }
+            })
         res.json(usuarios)
     } catch (error) {
         console.log(error)
@@ -18,12 +22,24 @@ const getUserId = async(req, res) => {
     try {
         const { id } = req.params;
         const peticion = await db.collection("usuarios").doc(id).get();
-        const usuario =  {id: id, datos: peticion.data()}
+        let data = peticion.data()
+        const usuario =  {
+            id: id,
+            ...data
+        }
         res.json(usuario)
     } catch (error) {
         console.log(error)
     }
 };
+
+//Trabajo tirado a la basura por Santiago, GRACIAS!
+// const usuarios = docs.map(usuario => ({
+        //     id:usuario.id,
+        //     datos:usuario.data()
+        // }));
+
+//Para que le mostré, mala mía, me mandé un mocaso!
 
 
 
