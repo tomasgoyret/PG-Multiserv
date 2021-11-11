c51927f26a673d1db2e0a8a596af7e14c220a768
Luis Aguilar <luisenrique.aguilares@gmail.com>
2021-11-11 09:35:06 -0600

    Componentes:
    Added atomic design folders and components

--- Multiserv/client/src/App.jsx
+++ Multiserv/client/src/App.jsx
@@ -1,11 +1,12 @@
 import { Route, Routes } from "react-router";
 import Root from "./pages/Root/Root";
-
+import Components from "./pages/Components/Components";
 function App() {
   return (
     <div>
       <Routes>
         <Route path="/" element={<Root />} />
+        <Route path="/components" element={<Components />} />
       </Routes>
     </div>
   );
