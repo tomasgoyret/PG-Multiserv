4d4927663e4bc45e989befc037375ab962f4eb9a
MaarGoesGil <abrilmartinagil@gmail.com>
2021-11-11 18:44:00 -0300

    Ruta creada de orderBy , por query llega el orden requerido, a-z z-a puntuacion, de todos los servicios

--- Multiserv/api/src/routes/index.js
+++ Multiserv/api/src/routes/index.js
@@ -5,12 +5,13 @@ const getUsersRouter = require("./getUsers");
 const deleteUserRouter = require("./deleteUser");
 const getServRouter = require("./getServices");
 const getFilterRouter = require("./getFilter.js");
+const getOrderByRouter = require("./getOrderBy");
 
 router.use("/agregar-usuario", postUserRouter); 
 router.use("/api", getUsersRouter);
 router.use("/eliminar-usuario", deleteUserRouter); 
 router.use("/services", getServRouter);
 router.use("/filter", getFilterRouter); 
-
+router.use("/orderby", getOrderByRouter);
 
 module.exports = router;
