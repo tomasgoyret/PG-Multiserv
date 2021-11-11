cdd195df972f06c503dcce36a766c0ee32895c3a
MaarGoesGil <abrilmartinagil@gmail.com>
2021-11-11 17:12:14 -0300

    se agrego ruta para servicios: buscar por id, buscar onSearch y entregar todos los servicios

--- Multiserv/api/src/routes/index.js
+++ Multiserv/api/src/routes/index.js
@@ -2,12 +2,13 @@ const { Router } = require('express');
 const router = Router();
 const postUserRouter = require('./addUser');
 const getUsersRouter = require("./getUsers");
-const deleteUserRouter = require("./deleteUser")
+const deleteUserRouter = require("./deleteUser");
+const getServRouter = require("./getServices");
 
-router.use("/agregar-usuario", postUserRouter);
+router.use("/agregar-usuario", postUserRouter); 
 router.use("/api", getUsersRouter);
-router.use("/eliminar-usuario", deleteUserRouter);
-
+router.use("/eliminar-usuario", deleteUserRouter); 
+router.use("/services", getServRouter);
 
 
 module.exports = router;
