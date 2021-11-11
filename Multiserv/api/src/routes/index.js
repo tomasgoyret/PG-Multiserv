371f432fc9f72f55f911e9c1fd00772198678d78
MaarGoesGil <abrilmartinagil@gmail.com>
2021-11-11 17:18:47 -0300

    Agrego de ruta filter, filtra por atributo de services por la query que le envien

--- Multiserv/api/src/routes/index.js
+++ Multiserv/api/src/routes/index.js
@@ -4,11 +4,13 @@ const postUserRouter = require('./addUser');
 const getUsersRouter = require("./getUsers");
 const deleteUserRouter = require("./deleteUser");
 const getServRouter = require("./getServices");
+const getFilterRouter = require("./getFilter.js");
 
 router.use("/agregar-usuario", postUserRouter); 
 router.use("/api", getUsersRouter);
 router.use("/eliminar-usuario", deleteUserRouter); 
 router.use("/services", getServRouter);
+router.use("/filter", getFilterRouter); 
 
 
 module.exports = router;
