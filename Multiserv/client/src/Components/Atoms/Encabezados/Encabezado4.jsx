bbeb3e78207bd04b3b3d34f94c1e22220a3972ec
Santiago <santisalxe@gmail.com>
2021-11-11 21:09:58 -0500

    Flow Atomic Design

---
+++ Multiserv/client/src/Components/Atoms/Encabezados/Encabezado4.jsx
@@ -0,0 +1,9 @@
+const Encabezado4 = ({ clases, titulo }) => {
+    return (
+        <div>
+            <h4 className={`${clases} text-3xl font-semibold`}>{titulo}</h4>
+        </div>
+    )
+}
+
+export default Encabezado4
