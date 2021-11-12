fe33124d8703b95ff18fc039c1d33ba5ea660fb1
bruno8a10 <bruno8a108a@gmail.com>
2021-11-12 18:07:55 -0300

    se agregaron 2 rutas

--- Multiserv/client/src/App.jsx
+++ Multiserv/client/src/App.jsx
@@ -2,6 +2,8 @@ import { Route, Routes } from "react-router";
 import Root from "./pages/Root/Root";
 import SignUp from "./pages/SignUp/SignUp";
 import Components from "./pages/Components/Components";
+import PasswordChange from "./pages/PasswordChange/PasswordChange";
+import PasswordReset from "./pages/PasswordReset/PasswordReset"
 
 function App() {
   return (
@@ -10,6 +12,8 @@ function App() {
         <Route path="/" element={<Root />} />
         <Route path="/signup" element={<SignUp />} />
         <Route path="/components" element={<Components />} />
+        <Route path="/passwordChange" element={<PasswordChange />} />
+        <Route path="/passwordReset" element={<PasswordReset />} />
       </Routes>
     </div>
   );
