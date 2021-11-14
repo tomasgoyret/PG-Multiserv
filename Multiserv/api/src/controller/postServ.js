fea26a4942e15cde339dad53531b68d965f2212c
MaarGoesGil <abrilmartinagil@gmail.com>
2021-11-14 09:17:01 -0300

    Agregado de deleteServ y putServ

--- Multiserv/api/src/controller/postServ.js
+++ Multiserv/api/src/controller/postServ.js
@@ -2,8 +2,8 @@ const { db } = require("../db.js");
 
 
 const postServ = async (req, res) => {
-    const { category, description, photos, priceRange, rating, title, uidUser, currency } = req.body;
-    const newService = { category, description, photos, priceRange, rating, title, uidUser, currency}
+    const { category, description, photos, min, max, rating, title, uidUser, currency } = req.body;
+    const newService = { category, description, photos, min, max, rating, title, uidUser, currency}
     try {
         const service = await db.collection("services").add(newService);
         const serviceUpdate = await db.collection('services').doc(service.id).update({
