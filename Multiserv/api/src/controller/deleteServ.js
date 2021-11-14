c02f25b9fbcc0603602f721c57377a30547e15c1
MaarGoesGil <abrilmartinagil@gmail.com>
2021-11-14 09:16:09 -0300

    Agregado de deleteServ y putServ

---
+++ Multiserv/api/src/controller/deleteServ.js
@@ -0,0 +1,14 @@
+const { db } = require("../db.js");
+
+const deleteServ = async (req, res) => {
+    const {id} = req.params;
+    try {
+        await db.collection('services').doc(id).delete();
+        res.send("El servicio se eliminó correctamente")
+    }
+    catch (error) {
+        res.send(error)
+    }
+}
+
+module.exports = deleteServ;
--- Multiserv/api/src/routes/getServices.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const { getServices, getServicesId} = require("../controller/getServ");
-
-router.get("/", getServices);
-router.get("/:id", getServicesId);
-
-module.exports = router;
