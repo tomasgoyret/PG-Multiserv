6fb3d1fb3625f9e7680f3ca42282514ffd2a453b
Bastian Alzate <44325651+bastianalzate@users.noreply.github.com>
2021-11-17 10:49:11 -0500

    creo la pagina y el componente profile, y los coloco en el app

---
+++ Multiserv/client/src/pages/Profile/Profile.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Profile = () => {
+    return(
+        <div className="w-full h-screen border-2">
+            
+        </div>
+    )
+}
+
+export default Profile;
