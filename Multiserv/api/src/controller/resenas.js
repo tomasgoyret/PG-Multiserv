489c43bb5de7e60cd62bbb6775dae424f01bfbe3
Ulises Vargas <uli.vargas02@gmail.com>
2021-11-21 14:37:17 -0600

    Agrego ruta de put, delete y actualizo la ruta de post para que solo se cree una reseña por usuario

--- Multiserv/api/src/controller/resenas.js
+++ Multiserv/api/src/controller/resenas.js
@@ -8,15 +8,24 @@ const postResena= async (req, res)=> {
             title, details, rating
         };
         try{ 
-            const resena = await Resenas.create(newResena);
-            const servicio = await Servicios.findByPk(id); 
-            const usuario = await Usuarios.findOne({where: {uidClient}}); 
-            await servicio.addResenas(resena);
-            await usuario.addResenas(resena);
+            const [resena, created] = await Resenas.findOrCreate({
+                where: {usuarioUidClient: uidClient},
+                defaults: {
+                    title,
+                    details,
+                    rating
+                }
+            });
+            if(created) {
+                const servicio = await Servicios.findByPk(id); 
+                const usuario = await Usuarios.findOne({where: {uidClient}}); 
+                await servicio.addResenas(resena);
+                await usuario.addResenas(resena);
+            }
+            if(!created) return res.status(404).json({msg: "Solo puede hacer una reseña por servicio"})
             res.status(200).json({msg:"La reseña se creo correctamente", data: resena})
 
         } catch(err){
-            console.log(err)
             res.status(400).send(err)
         }
 }
@@ -36,7 +45,35 @@ const getResenas = async (req, res) => {
     }
 }
 
+const deleteResenas = async (req, res) => {
+        const id = req.params;
+        const resena = await Resenas.destroy({where: id});
+        resena ? res.json({msg: "La reseña se borró correctamente"}) : 
+        res.json({msg: 'La reseña que intenta eliminar no existe'});
+};
+
+
+const putResenas = async (req, res) => {
+    try {
+        const { id } = req.params;
+        const {title, details, rating} = req.body;
+
+        const resena = await Resenas.update({
+            title,
+            details,
+            rating
+        }, {where: {
+            id
+        }});
+        res.status(201).json({msg: "La reseña fue actualziada"})
+    } catch (error) {
+        console.log(error);
+    }
+};
+
 module.exports= {
     postResena,
-    getResenas
+    getResenas,
+    deleteResenas,
+    putResenas
 }
