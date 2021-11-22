1f8e8541c77cd4d50c6b7602abd97319d0efb31a
Luis Aguilar <luisenrique.aguilares@gmail.com>
2021-11-21 18:07:45 -0600

    Funcionalidad completa del form de crearServicio

--- Multiserv/client/src/pages/CreateService/CreateService.jsx
+++ Multiserv/client/src/pages/CreateService/CreateService.jsx
@@ -10,10 +10,15 @@ import Button from '../../Components/Atoms/Button/Button';
 import { FaPlus, FaTimes } from "react-icons/fa";
 import Image from '../../Components/Atoms/Image/Image';
 import axios from 'axios';
+import { HiOutlineArrowNarrowRight } from "react-icons/hi";
+import { BiLoader } from "react-icons/bi";
+import Swal from 'sweetalert2';
 
 const CreateService = () => {
     const [disabledNext, setDisabledNext] = useState(true)
     const categoriasDb = useSelector((state) => state.categories)
+    const [loadingSave, setLoadingSave] = useState(false)
+    const [loadingPayment, setLoadingPayment] = useState(false)
     const navigate = useNavigate();
     const dispatch = useDispatch()
     let datosSesionFromLocalStorage = JSON.parse(localStorage.getItem("datoSesion"))
@@ -132,40 +137,55 @@ const CreateService = () => {
         }
     }
 
-    const [nuevServ, setnuevoServ] = useState({})
+    const [nuevServ, setnuevoServ] = useState(null)
 
     useEffect(async ()=>{
             if (stepForm === 3){
-            let nuevoServ = {
-            title : service.title,  
-            currency: service.currency,  
-            category: service.categorias, 
-            description: service.description, 
-            max: service.max, 
-            min: service.min, 
-            uidClient: uid,
-            photos: service.img
+                setLoadingSave(true)
+                if (!nuevServ) {
+                    let nuevoServ = {
+                        title: service.title,
+                        currency: service.currency,
+                        category: service.categorias,
+                        description: service.description,
+                        max: service.max,
+                        min: service.min,
+                        uidClient: uid,
+                        photos: service.img
+                    }
+                    try {
+                        let serv = await axios.post(`newservice`, nuevoServ)
+                        setLoadingSave(false)
+                        setnuevoServ(serv.data.servicio)
+                        console.log("se creó el servicio")
+                    } catch (err) {
+                        setLoadingSave(false)
+                        console.log(err)
+                    }
+                } else {
+                    setLoadingSave(false)
+                }
             }
-            try {
-                let serv = await axios.post (`newservice`,nuevoServ)
-                setnuevoServ(serv.data.servicio)
-                console.log("se creó el servicio")
-            } catch (err) {
-                console.log(err)
-            }
-        }
     },[stepForm])
 
     const [link, setLink] = useState("")
 
     const linkPago = async (uid) => {
+        setLoadingPayment(true)
         try {
             let link = await axios.post(`pay-service`, {id : `${uid}`})
             setLink(link.data)
+            setLoadingPayment(false)
             window.open(link.data)
            // window.location.href = "/home"
             console.log(link.data)
         } catch (err) {
+            Swal.fire({
+                title: 'Error!',
+                text: err.data,
+                icon: 'error',
+                confirmButtonText: 'X'
+            })
             console.log(err)
         }
 
@@ -177,7 +197,7 @@ const CreateService = () => {
                 style={{ height: '36rem', width: '100%' }}
                 className="rounded-md bg-white mx-8 py-4 px-6 flex flex-col justify-between items-center overflow-y-auto custom-scrollbar">
                 <div id="status" className="w-full text-center">
-                    <h1 className="text-4xl font-semibold text-cyan-900 mb-6">Crear un nuevo servicio</h1>
+                    <h1 className="text-4xl font-semibold text-cyan-900 mb-6 border-b border-gray-100">Crear un nuevo servicio</h1>
                     <div id="progress-description" className="flex flex-row mb-2">
                         <div id="step1" className="w-full flex justify-center" >
                             <div className={`px-4 rounded-full font-semibold text-white bg-cyan-900 ${stepForm >= 1 ? 'bg-cyan-900 text-white' : 'bg-purple-200 text-cyan-900'}`}>
@@ -190,8 +210,9 @@ const CreateService = () => {
                             </div>
                         </div>
                         <div id="step3" className="w-full flex justify-center" >
-                            <div className={`px-4 -ml-3 rounded-full font-semibold text-white bg-cyan-900 ${stepForm === 3 ? 'bg-cyan-900 text-white' : 'bg-purple-200 text-cyan-900'}`}>
-                                <span>Pagar y finalizar</span>
+                            <div className={`px-4 -ml-3 rounded-full font-semibold text-white bg-cyan-900 ${stepForm === 3 ? 'bg-cyan-900 text-white' : 'bg-purple-200 text-cyan-900'} transition-all duration-300 inline-flex flex-row`}>
+                                {loadingSave && <BiLoader className="self-center animate-spin mr-2" />}
+                                <span className="self-center" >Pagar y finalizar</span>
                             </div>
                         </div>
                     </div>
@@ -378,14 +399,49 @@ const CreateService = () => {
                                 </>
                             )}
                     </div>
-                    <div id="step3" className={`${stepForm === 3 ? 'flex mt-3 justify-center items-center' : 'hidden'} w-full`}>
-                        <div className="w-1/2 flex flex-col">
-                            <div className="my-4">
-                                <span className='text-md font-semibold text-2xl text-cyan-900'>Titulo</span>
-                                <span className='text-md'>{service.title}</span>
+                    <div id="step3" className={`${stepForm === 3 ? 'flex mt-3 justify-start items-start' : 'hidden'} w-full`}>
+                        <div className="w-1/2 flex flex-col justify-start items-start  pr-4 border-r border-gray-300">
+                            <div className="mb-6 border-b border-gray-300 w-full flex flex-row">
+                                <span className='text-xl text-gray-700 font-semibold'>Datos del servicio <span><HiOutlineArrowNarrowRight className="ml-3 inline-flex" /></span></span>
+                            </div>
+                            <div className="pb-2 mb-2 w-full border-b border-gray-100">
+                                <span className='text-md font-semibold text-lg text-cyan-900 mr-3'>Categoría:</span>
+                                <span className='text-md text-gray-50 bg-sky-700 px-4 py-0.5 rounded-full font-semibold'>{service.categorias[0]}</span>
+                            </div>
+                            <div className="pb-2 mb-2 w-full border-b border-gray-100">
+                                <span className='text-md font-semibold text-lg text-cyan-900 mr-3'>Titulo:</span>
+                                <span className='text-md text-gray-700 font-semibold'>{service.title}</span>
+                            </div>
+                            <div className="pb-2 mb-2 w-full border-b border-gray-100">
+                                <span className='text-md font-semibold text-lg text-cyan-900 mr-3'>Descripción:</span>
+                                <span className='text-md text-gray-700 font-semibold'>{service.description}</span>
+                            </div>
+                            <div className="pb-2 mb-2 w-full border-b border-gray-100 flex flex-row">
+                                <div className="mr-4">
+                                    <span className='text-md font-semibold text-lg text-cyan-900 mr-3'>Precio mínimo:</span>
+                                    <span className='text-md text-gray-700 font-semibold'>${service.min} <span className="text-gray-400">({service.currency})</span> </span>
+                                </div>
+                                <div className="">
+                                    <span className='text-md font-semibold text-lg text-cyan-900 mr-3'>Precio máximo:</span>
+                                    <span className='text-md text-gray-700 font-semibold'>${service.max} <span className="text-gray-400">({service.currency})</span> </span>
+                                </div>
+                            </div>
+
+                        </div>
+                        <div className="w-1/2 pl-4 ">
+                            <div className="flex flex-col h-full">
+                                <div className="mb-3 border-b border-gray-300 flex flex-row">
+                                    <span className='text-md font-semibold text-xl text-cyan-900'>Imagen de portada <HiOutlineArrowNarrowRight className="ml-3 inline-flex" /></span>
+                                </div>
+                                <div className="">
+                                    <Image
+                                        name="photo1"
+                                        imagen={img}
+                                        imgClass={`object-cover rounded-lg h-64 `}
+                                    />
+                                </div>
                             </div>
                         </div>
-                        <div className="w-1/2"></div>
                     </div>
                 </div>
                 <div id="buttons" className="flex w-full flex-row justify-between px-48 border-t border-gray-200 mt-4 pt-4">
@@ -398,16 +454,18 @@ const CreateService = () => {
                         disabled={stepForm === 1}
                     />
                     {stepForm !== 3 && <Button
-                        disabled={disabledNext}
+                        disabled={disabledNext || (stepForm === 2 && img === null)}
                         text="Siguiente"
                         customTextColor="#FFFFF"
                         theme="#155E75"
                         action={addStep}
                     />}
                     {stepForm === 3 && <Button
-                        text="Pagar"
+                        icon={loadingPayment && <BiLoader className="self-center animate-spin mr-2" />}
+                        disabled={loadingPayment}
+                        text="Continuar en MercadoPago"
                         customTextColor="#FFFFF"
-                        theme="#155E75"
+                        theme="#00a5ec"
                         action={()=>{linkPago(nuevServ.id)}}
                     />}
 
