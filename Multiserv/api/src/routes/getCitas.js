bcefc8d5830c9caef2f97e50f2c3cee9c787e5e3
MaarGoesGil <abrilmartinagil@gmail.com>
2021-11-23 18:08:02 -0300

    Fix

--- Multiserv/api/src/routes/getCitas.js
+++ Multiserv/api/src/routes/getCitas.js
@@ -2,6 +2,6 @@ const express = require("express");
 const router = express.Router();
 const getCitas = require('../controller/getCitas');
 
-router.getCitas('/:uidClient', getCitas);
+router.get('/:uidClient', getCitas);
 
 module.exports = router;
