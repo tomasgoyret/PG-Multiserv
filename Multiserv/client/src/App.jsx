3d9187fc932bdb935bb0111a12151d6ef2fcd0f6
Abril♥ <abrilmartinagil@gmail.com>
2021-11-25 19:04:38 -0300

    Revert "Abril"

---
+++ Multiserv/client/src/App.jsx
@@ -0,0 +1,170 @@
+import { useRoutes } from "react-router";
+import { toast, ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import LandingPage from './pages/LandingPage/LandingPage'
+import SignUp from "./pages/SignUp/SignUp";
+import Components from "./pages/Components/Components";
+import PasswordReset from "./pages/PasswordReset/PasswordReset"
+import Home from "./pages/Home/Home";
+import DetalleServicio from "./pages/DetalleServicio/DetalleServicio";
+import DetalleProveedor from "./pages/DetalleProveedor/DetalleProveedor";
+import VerifyEmail from "./pages/VerifyEmail/VerifyEmail";
+import UsersValidations from "./pages/UsersValidations/UsersValidations";
+import PasswordChange from "../src/pages/PasswordChange/PasswordChange";
+import ReviewService from "./Components/Organisms/ReviewService/ReviewService";
+import SignInAdmin from "./pages/SignInAdmin/SignInAdmin";
+import ControlPanel from "./pages/ControlPanel/ControlPanel";
+import CreateService from "./pages/CreateService/CreateService";
+import HomeNavigation from "./Components/Organisms/HomeNavigation/HomeNavigation";
+import Profile from "./pages/Profile/Profile";
+import ListFavorites from "./pages/ListFavorites/ListFavorites";
+import ConfirmServicio from "./pages/ConfirmServ/ConfirmServ";
+import MyServices from "./pages/MyServices/MyServices";
+import EditarServicio from "./pages/EditarServicio/EditarServicio";
+import MisCitas from "./pages/MisCitas/MisCitas";
+import Horarios from "./pages/Horarios/Horarios";
+import { MapServices } from "./pages/MapServices/MapServices";
+
+function App() {
+  toast.configure()
+  const routes = [
+    {
+      index: true,
+      element: <LandingPage />
+    },
+    {
+      path: '/signup',
+      element: <SignUp />
+    },
+    {
+      path: '/components',
+      element: <Components />
+    },
+    {
+      path: 'home',
+      element: <HomeNavigation />,
+      children: [
+        {
+          index: true,
+          element: <Home />
+        },
+        {
+          path: '/home/mapservices',
+          element: <MapServices />
+        },
+        {
+          path: '/home/:uidClient/new-service',
+          element: <CreateService />
+        },
+        {
+          path: '/home/:uidClient/list-favorites',
+          element: <ListFavorites />
+        },
+        {
+          path: '/home/chat',
+          element: <div><h1>chat</h1></div>
+        },
+        {
+          path: '/home/schedule',
+          element: <div><h1>schedule</h1></div>
+        },
+        {
+          path: '/home/detalleServicio/:id',
+          element: <DetalleServicio />
+        },
+        {
+          path: '/home/:uidClient/my-services',
+          element: <MyServices />
+        },
+        {
+          path: '/home/:uidClient/appointments',
+          element: <MisCitas />
+        },
+        {
+          path: '/home/:idService/Horarios',
+          element: <Horarios />
+        },
+        {
+          path: '/home/servicios/:id/editar',
+          element: <EditarServicio />
+        },
+      ]
+    },
+    {
+      path: '/passwordReset',
+      element: <PasswordReset />
+    },
+    {
+      path: '/detalleProveedor/:id',
+      element: <DetalleProveedor />
+    },
+    {
+      path: '/email-verification',
+      element: <VerifyEmail />
+    },
+    {
+      path: '/user-validations',
+      element: <UsersValidations />
+    },
+    {
+      path: '/profile',
+      element: <Profile />
+    },
+    {
+      path: '/confirmServ',
+      element: <ConfirmServicio />
+    },
+    {
+      path: '/control-panel',
+      element: <ControlPanel />
+    },
+  ]
+  /*
+  <div>
+      <Routes>
+        <Route path="/"/>
+        <Route path="/signup"/>
+        <Route path="/components" />
+        <Route path='/home'> 
+          <Route path='/home/chat'/>
+
+          <Route path='/home/profile'/>
+          <Route path='/home/schedule'/>
+
+        </Route>
+        <Route path="/passwordChange"/>
+        <Route path="/passwordChange" element={<PasswordChange />} /> 
+        <Route path="/passwordReset" element={<PasswordReset />} />
+        <Route path="/detalle" element={<Detalle />} />
+        <Route path="/test" element={<PasswordChange />} />
+        <Route path="/email-verification" element={<VerifyEmail />} />
+        <Route path="/user-validations" element={<UsersValidations />} />
+        <Route path="/review" element={<ReviewService />} />
+        <Route path="/admin" element={<SignInAdmin />} />
+        <Route path="/control-panel" element={<ControlPanel />} />
+         <Route path="/pago" element={<Pago/>} />
+        <Route path="/passwordChange"/>
+        <Route path="/passwordReset"/>
+        <Route path="/detalleServicio/:id"/>
+        <Route path="/detalleProveedor/:id"/>
+        <Route path="/profile" />
+        <Route path="/test" />
+        <Route path="/email-verification"/>
+        <Route path="/user-validations" />
+        <Route path="/pago"/>
+      </Routes>
+    <div className="custom-scrollbar">
+      {routing}
+    </div>
+    </div>
+   */
+  let routing = useRoutes(routes);
+  return (
+    <div>
+      {routing}
+      <ToastContainer />
+    </div>
+  );
+}
+
+export default App;
---
+++ Multiserv/client/src/redux/reducers/reducers.js
@@ -0,0 +1,219 @@
+import {
+    BUSCAR,
+    ORDERALPH,
+    ORDERRAT,
+    SERVICIOS,
+    USUARIOS,
+    RESETORDER,
+    FILTERCAT,
+    GETCATS,
+    SERVICIOID,
+    EMPATYSERVICIOID,
+    USUARIOID,
+    EMPATYUSUARIO,
+    FAVORITES,
+    ELIMINARFAVORITES,
+    CLIENTES_BUSCADOS,
+    PROVEDORES_BUSCADOS,
+    SERVICIOS_BUSCADOS,
+    CATEGORIAS_BUSCADAS,
+    REVIEWS,
+    MIS_SERVICIOS,
+    ELIMINAR_MISERVICIO,
+    EDITAR_MISERVICIO,
+    MIS_CITAS,
+    MAPSERVICES,
+} from "../actionTypes/actionTypes";
+
+/* Estado global */
+const initalState = {
+    loadingServices: true,
+    loadingServicesDetalle: true,
+    loadingProveedorDetalle: true,
+    servicios: [],
+    usuarios: [],
+    servXUser: [],
+    aux: [],
+    categories: [],
+    detalleServicio: {},
+    misFavoritos: [],
+    provedoresBuscados: [],
+    clientesBuscados: [],
+    serviciosBuscados: [],
+    categoriasBuscadas: [],
+    detalleUsuario: {},
+    reviews: [],
+    misServicios: [],
+    misCitas: [],
+    mapServices: [],
+}
+
+function rootReducer(state = initalState, { type, payload }) {
+    switch (type) {
+        case SERVICIOS:
+            return {
+                ...state,
+                loadingServices: false,
+                servicios: payload,
+                aux: payload
+            }
+        case MIS_CITAS:
+            return{
+                ...state,
+                misCitas:payload
+            }
+        case BUSCAR:
+            let newServ = state.aux.filter(serv => serv.title.toLowerCase().includes(payload.toLowerCase()))
+            return {
+                ...state,
+                servicios: newServ
+            }
+        case MAPSERVICES:
+            console.log(payload)
+            return {
+                ...state,
+                mapServices: payload
+            }
+        case GETCATS:
+            return {
+                ...state,
+                categories: payload
+            }
+        case FILTERCAT:
+            let filteredCat = state.aux.filter(serv => {
+                if(serv.categorias[0] !== undefined){
+                    return serv.categorias[0].title?.toLowerCase() === payload.toLowerCase()
+                }
+            })
+            return {
+                ...state,
+                servicios: filteredCat
+            }
+        case ORDERALPH:
+            let orderedAlph = state.servicios
+            if (payload === 'asc') {
+                orderedAlph.sort((prev, post) => {
+                    if (prev.title < post.title) return -1
+                    else if (prev.title > post.title) return 1
+                    else return 0
+                })
+            }
+            else if (payload === 'desc') {
+                orderedAlph.sort((prev, post) => {
+                    if (prev.title.toLowerCase() < post.title.toLowerCase()) return 1
+                    else if (prev.title.toLowerCase() > post.title.toLowerCase()) return -1
+                    else return 0
+                })
+            }
+            return {
+                ...state,
+                servicios: orderedAlph
+            }
+        case ORDERRAT:
+            let orderedRat = state.servicios;
+            if (payload === 'asc') {
+                orderedRat.sort((prev, post) => prev.rating - post.rating)
+            }
+            else if (payload === 'desc') {
+                orderedRat.sort((prev, post) => post.rating - prev.rating)
+            }
+            return {
+                ...state,
+                servicios: orderedRat
+            }
+        case USUARIOS:
+            return {
+                ...state,
+                usuarios: payload
+            }
+        case RESETORDER:
+            return {
+                ...state,
+                servicios: state.aux
+            }
+        case EMPATYUSUARIO:
+            return {
+                ...state,
+                loadingServices: false,
+                detalleUsuario: {},
+            }
+        case USUARIOID:
+            return {
+                ...state,
+                loadingProveedorDetalle: false,
+                detalleUsuario: payload,
+            }
+        case FAVORITES:
+            return {
+                ...state,
+                misFavoritos: payload,
+            }
+        case ELIMINARFAVORITES:
+            return {
+                ...state,
+                misFavoritos: payload,
+            }
+
+        case EMPATYSERVICIOID:
+            return {
+                ...state,
+                loadingServices: false,
+                detalleServicio: {},
+            }
+        case SERVICIOID:
+            return {
+                ...state,
+                loadingServicesDetalle: false,
+                detalleServicio: payload,
+            }
+        case CLIENTES_BUSCADOS:
+            const auxClientesBuscados = state.usuarios.filter(usuario => usuario.displayName.toLowerCase().includes(payload.toLowerCase()))
+            return {
+                ...state,
+                clientesBuscados: auxClientesBuscados
+            }
+        case PROVEDORES_BUSCADOS:
+            const auxProvedoresBuscados = state.usuarios.filter(usuario => usuario.displayName.toLowerCase().includes(payload.toLowerCase()))
+            return {
+                ...state,
+                provedoresBuscados: auxProvedoresBuscados
+            }
+        case SERVICIOS_BUSCADOS:
+            const auxServiciosBuscados = state.servicios.filter(servicio => servicio.title.toLowerCase().includes(payload.toLowerCase()))
+            return {
+                ...state,
+                serviciosBuscados: auxServiciosBuscados
+            }
+        case CATEGORIAS_BUSCADAS:
+            const auxCategoriasBuscadas = state.categories.filter(categoria => categoria.name.toLowerCase().includes(payload.toLowerCase()))
+            return {
+                ...state,
+                categoriasBuscadas: auxCategoriasBuscadas
+            }
+        case REVIEWS:
+            return {
+                ...state,
+                reviews: payload
+            }
+        case MIS_SERVICIOS:
+            return {
+                ...state,
+                misServicios: payload
+            }            
+        case ELIMINAR_MISERVICIO:
+            return {
+                ...state,
+                misServicios: payload
+            }
+        case EDITAR_MISERVICIO:
+            return {
+                ...state,
+                detalleServicio: payload
+            }
+
+        default:
+            return state;
+    }
+}
+
+export default rootReducer
