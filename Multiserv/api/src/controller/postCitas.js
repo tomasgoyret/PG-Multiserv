94038e9a51a1e01eae8f6c4e886f279fe4afa8a9
MaarGoesGil <abrilmartinagil@gmail.com>
2021-11-26 19:42:24 -0300

    Fix bugs

--- Multiserv/api/src/controller/postCitas.js
+++ Multiserv/api/src/controller/postCitas.js
@@ -3,10 +3,13 @@ const { Citas, Usuarios, Servicios } = require("../db.js");
 const postCitas = async (req, res) => {
     const { id } = req.params;
     const { dia, hora, uidClient, direccion, ciudad } = req.body;
+    console.log(req.body,'bodyyyy')
     try {
         const user = await Usuarios.findByPk(uidClient)
         const servicio = await Servicios.findByPk(id)
         const cita = { dia, hora, nameUser:user.displayName, direccion, ciudad }
+        
+    console.log(cita,'cita')
         const citas = await Citas.create(cita);
         await user.addCitas(citas)
         await servicio.addCitas(citas)
