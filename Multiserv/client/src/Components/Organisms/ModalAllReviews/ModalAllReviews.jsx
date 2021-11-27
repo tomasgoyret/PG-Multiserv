1428c6a13218dac09a1dd80d227377fea83b58d5
Ulises Vargas <uli.vargas02@gmail.com>
2021-11-26 18:24:51 -0600

    Se agrega una barra de promedio de calificaciones de reseñas de cada servicio

--- Multiserv/client/src/Components/Organisms/ModalAllReviews/ModalAllReviews.jsx
+++ Multiserv/client/src/Components/Organisms/ModalAllReviews/ModalAllReviews.jsx
@@ -1,12 +1,13 @@
 import React from "react";
+import ProgressRating from "../../Molecules/ProgressBar/ProgressBar";
 import ReviewService from "../ReviewService/ReviewService";
 
 const ModalAllReviews = () => {
     return(
         <div className="flex justify-center items-center w-full h-screen bg-gray-800 bg-opacity-70">
-            <div className="w-4/6 h-5/6 bg-white overflow-y-scroll rounded-md ">
-                <div className="w-full h-2/6 border-2">
-                    Rating
+            <div className="w-4/6 h-5/6 bg-white overflow-y-scroll rounded-md">
+                <div className="w-full border-2">
+                    <ProgressRating />
                 </div>
                 <div>
                     <ReviewService />
---
+++ Multiserv/client/src/Hooks/useReviews.js
@@ -0,0 +1,52 @@
+import { useSelector } from "react-redux";
+
+
+export const Review = () => {
+    const { reviews, usuarios } = useSelector(state => state)
+    
+    const reviewsdata = reviews.map(review => ({
+        title: review.title,
+        details: review.details,
+        rating: review.rating,
+        user: usuarios.filter(usuario => usuario.uidClient === review.usuarioUidClient)
+    }));
+    
+    const rating = reviewsdata.map(rating => rating.rating);
+    const total = rating.reduce((a, b) => a + b, 0);
+    const promedio = total / rating.length;
+
+
+    const titleEstrellas = ["1 Estrella", "2 Estrellas", "3 Estrellas", "4 Estrellas", "5 Estrellas"]
+
+    
+    
+    const unaEstrella = rating.filter(r => r === 1)
+    const dosEstrella = rating.filter(r => r === 2)
+    const tresEstrella = rating.filter(r => r === 3)
+    const cuatroEstrella = rating.filter(r => r === 4)
+    const cincoEstrella = rating.filter(r => r === 5)
+    
+    const estrellas = {
+        unaEstrella: unaEstrella.length,
+        dosEstrella: dosEstrella.length,
+        tresEstrella: tresEstrella.length,
+        cuatroEstrella: cuatroEstrella.length,
+        cincoEstrella: cincoEstrella.length
+
+    }
+    const estrellasData = Object.values(estrellas)
+    const porcentaje = estrellasData.map(p => ((p / rating.length) * 100))
+    let title = titleEstrellas.values();
+
+
+
+    return {
+        promedio,
+        rating,
+        reviewsdata,
+        total,
+        reviewsdata,
+        porcentaje,
+        title
+    }
+};
