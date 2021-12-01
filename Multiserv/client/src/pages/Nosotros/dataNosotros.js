af85c795fa7a6e1094cc2ac9e7dbbff50ad62cf1
Robmargar <roberto.mar.garc@gmail.com>
2021-11-30 18:09:01 -0600

    Foto de Ale

--- Multiserv/client/src/pages/Nosotros/dataNosotros.js
+++ Multiserv/client/src/pages/Nosotros/dataNosotros.js
@@ -2,6 +2,7 @@ import linkedin from "../../../src/assets/images/linkedin2.png"
 import github from "../../../src/assets/images/github.png"
 import bastian from "../../../src/assets/fotos/bastian.jpeg"
 import tomas from "../../../src/assets/fotos/tomas.jpeg"
+import ale from "../../../src/assets/fotos/ale.jpg"
 
 const dataNosotros = [
     {
@@ -22,7 +23,7 @@ const dataNosotros = [
         ]
     },
     {
-        imagen: "",
+        imagen: ale,
         displayName: "Ale",
         titulo: "Full Stack Developer",
         redes: [
